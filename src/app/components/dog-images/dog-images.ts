fe709493c17b7b9f26911b6c4e9ba97b7a6d781e
Guilherme Moreira <guilherme.moreira1108@gmail.com>
2026-02-05 23:39:05 -0300

    estilizacao finalizada, arrumar bug das imagens

--- src/app/components/dog-images/dog-images.ts
+++ src/app/components/dog-images/dog-images.ts
@@ -1,6 +1,9 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { BreedSearch } from '../../services/breed-search';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { BreedSearch } from '../../services/breed-search';
+import { switchMap } from 'rxjs';
+switchMap;
 
 @Component({
   selector: 'app-dog-images',
@@ -9,16 +12,33 @@ import { CommonModule } from '@angular/common';
   templateUrl: './dog-images.html',
   styleUrl: './dog-images.css',
 })
-export class DogImages implements OnChanges {
-  constructor(private breedSearch: BreedSearch) {}
-
-  @Input() breed!: string;
+export class DogImages implements OnInit {
   images: string[] = [];
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['breed'] && this.breed)
-      this.breedSearch.getData(this.breed).subscribe((response: any) => {
+  constructor(
+    private route: ActivatedRoute,
+    private breedSearch: BreedSearch,
+  ) {}
+
+  ngOnInit() {
+    // O segredo é garantir que sempre que o parâmetro mudar, a busca dispare
+    this.route.params.subscribe((params) => {
+      const breed = params['breed']; // Pega o nome da raça da URL
+      if (breed) {
+        this.fetchImages(breed);
+      }
+    });
+  }
+
+  fetchImages(breed: string) {
+    this.breedSearch.getData(breed).subscribe({
+      next: (response: any) => {
         this.images = response.message;
-      });
+      },
+      error: (err) => {
+        console.error('Erro ao buscar imagens', err);
+        this.images = []; // Limpa as imagens se der erro
+      },
+    });
   }
 }
--- src/app/pages/breed-page/breed-page.ts
+++ src/app/pages/breed-page/breed-page.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { BreedSearch } from '../../services/breed-search';
-import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule, Location } from '@angular/common';
 import { DogImages } from '../../components/dog-images/dog-images';
+
 @Component({
   selector: 'app-breed-page',
   imports: [DogImages, CommonModule],
@@ -10,21 +9,7 @@ import { DogImages } from '../../components/dog-images/dog-images';
   styleUrl: './breed-page.css',
 })
 export class BreedPage {
-  constructor(
-    private breedSearch: BreedSearch,
-    private router: Router,
-    private route: ActivatedRoute,
-    private location: Location,
-  ) {}
-
-  breed!: string;
-
-  ngOnInit() {
-    const value = this.route.snapshot.paramMap.get('breed');
-    if (value) {
-      this.breed = value;
-    }
-  }
+  constructor(private location: Location) {}
 
   goBack() {
     this.location.back();
